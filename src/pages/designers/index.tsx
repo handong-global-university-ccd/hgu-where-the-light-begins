0b35bd0aa87593dd27157466356fe6ba749fd8e1
codms0201 <chanyeol0615@naver.com>
2025-10-27 08:14:34 +0900

    fix: fix work page(web/app)

--- src/pages/designers/index.tsx
+++ src/pages/designers/index.tsx
@@ -26,6 +26,9 @@ const designersData: Designer[] = [
   { id: '009', name: '강예은', major: 'Communication/Service' },
   { id: '010', name: '강주찬', major: 'UX' },
   { id: '011', name: '강하라', major: 'Communication/Service' },
+  { id: '012', name: '강예은', major: 'Communication/Service' },
+  { id: '013', name: '강주찬', major: 'UX' },
+  { id: '014', name: '강하라', major: 'Communication/Service' },
 ];
 
 export default function DesignersPage() {
@@ -59,79 +62,82 @@ export default function DesignersPage() {
   return (
     <div className="min-h-screen bg-white text-[#1C1C1C]">
       {/* Desktop Header */}
-      <div className="hidden lg:block">
+      <div className="hidden lg:block lg:sticky lg:top-0 lg:z-50">
         <Header />
       </div>
 
-      {/* MOBILE HEADER */}
-      <div className="flex lg:hidden items-center justify-between p-4">
-        {isMobileSearchOpen ? (
-          <input
-            type="text"
-            placeholder="Search"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className={`${suitMedium.className} w-[319px] text-[20px] text-[#7C7C7C] bg-transparent border-b border-[#7C7C7C] outline-none`}
-            autoFocus
-          />
-        ) : (
-          <h1 className={`${avantGarde.className} text-#1C1C1C text-[40px] font-[400]`}>Designers</h1>
-        )}
-        <div className="flex items-center gap-4">
-          <button onClick={() => setMobileSearchOpen(!isMobileSearchOpen)}>
-            {isMobileSearchOpen ? (
-               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
-                <path d="M8.99707 7.60938L16.5723 0.00195312L17.2812 0.708008L17.9902 1.41309L10.4082 9.02734L17.9902 16.6416L17.2812 17.3467L16.5723 18.0527L8.99707 10.4443L1.4209 18.0547L0.00292969 16.6445L7.58594 9.02734L0.00292969 1.41016L1.4209 0L8.99707 7.60938Z" fill="#1C1C1C"/>
-              </svg>
-            ) : (
-               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+      {/* MOBILE HEADER BLOCK */}
+      <div className="lg:hidden sticky top-0 z-50 bg-white">
+        <header className="flex items-center justify-between p-4">
+          <h1 className={`${avantGarde.className} text-[#1C1C1C] text-[40px] font-[400]`}>Designers</h1>
+          <div className="flex items-center gap-4">
+            <button onClick={() => setMobileSearchOpen(!isMobileSearchOpen)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
                 <path d="M9.59961 0.5C14.9015 0.5 19.2 4.79786 19.2002 10.0996L19.1875 10.5938C19.0902 12.5133 18.4279 14.2834 17.3652 15.7432L23.4609 21.8389C24.0467 22.4247 24.0467 23.3752 23.4609 23.9609C22.8753 24.5465 21.9256 24.5462 21.3398 23.9609L15.2432 17.8643C13.6588 19.0178 11.7093 19.7002 9.59961 19.7002L9.10645 19.6875C4.03387 19.4306 0 15.236 0 10.0996C0.000168531 4.79801 4.29795 0.500237 9.59961 0.5ZM9.59961 3.5C5.95477 3.50024 3.00017 6.4549 3 10.0996C3 13.7445 5.95467 16.7 9.59961 16.7002C13.2448 16.7002 16.2002 13.7446 16.2002 10.0996C16.2 6.45475 13.2446 3.5 9.59961 3.5Z" fill="#1C1C1C"/>
               </svg>
-            )}
-          </button>
-           {!isMobileSearchOpen && (
-              <Mobile_HeaderBtn />
-           )}
-        </div>
-      </div>
-
-      <div className="flex">
-        {/* 왼쪽 사이드바 (Desktop only) */}
-        <div className="hidden lg:block w-128 p-8 min-h-screen">
-          <h1 className={`${avantGarde.className} text-[70px] font-normal text-[#1C1C1C]`}>Designers</h1>
-          <div>
+            </button>
+            <Mobile_HeaderBtn />
+          </div>
+        </header>
+        {isMobileSearchOpen && (
+          <div className="p-4 pt-0 relative">
             <input
               type="text"
               placeholder="Search"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className={`${suitMedium.className} w-full text-[50px] font-normal bg-transparent border-none outline-none placeholder-[#BBB]`}
+              className={`${suitMedium.className} w-full text-[20px] text-[#7C7C7C] bg-transparent border-b border-[#7C7C7C] outline-none pb-2 pr-8`}
+              autoFocus
             />
+            <button
+              onClick={() => setMobileSearchOpen(false)}
+              className="absolute right-4 top-0 z-10"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
+                <path d="M8.99707 7.60938L16.5723 0.00195312L17.2812 0.708008L17.9902 1.41309L10.4082 9.02734L17.9902 16.6416L17.2812 17.3467L16.5723 18.0527L8.99707 10.4443L1.4209 18.0547L0.00292969 16.6445L7.58594 9.02734L0.00292969 1.41016L1.4209 0L8.99707 7.60938Z" fill="#1C1C1C"/>
+              </svg>
+            </button>
           </div>
-          <div className="w-full h-full bg-white">
-            {/* hoveredId가 있거나, 필터링된 디자이너가 1명일 때 이미지를 보여줍니다. */}
-            {hoveredId && (
-              <div className="relative w-4/5 h-full -mt-20">
-                <Image src={HoverImage} alt="Designer preview" fill className="object-contain" />
-              </div>
-            )}
+        )}
+
+      </div>
+
+      <div className="flex">
+        {/* 왼쪽 사이드바 */}
+        <div className="hidden lg:block w-128 lg:fixed lg:top-[147px] lg:h-[calc(100vh_-_147px)]">
+          <div className="h-full flex flex-col p-8">
+            <h1 className={`${avantGarde.className} text-[70px] font-normal text-[#1C1C1C]`}>Designers</h1>
+            <div>
+              <input
+                type="text"
+                placeholder="Search"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className={`${suitMedium.className} w-full text-[50px] font-normal bg-transparent border-none outline-none placeholder-[#BBB]`}
+              />
+            </div>
+            <div className="w-full flex-1 bg-white overflow-hidden">
+              {hoveredId && (
+                <div className="relative w-4/5 h-full -mt-20">
+                  <Image src={HoverImage} alt="Designer preview" fill className="object-contain" />
+                </div>
+              )}
+            </div>
           </div>
         </div>
-
         {/* 오른쪽 메인 콘텐츠 (Full width on mobile) */}
-        <div className="flex-1 bg-white lg:pt-18 w-full">
+        <div className="flex-1 bg-white lg:pt-18 w-full lg:ml-128">
           <div className="lg:px-8">
             <div className="border-b border-[#D2D2D2]">
               {filteredDesigners.map((designer, index) => {
                 const isHovered = hoveredId === designer.id;
                 const isPreviousHovered = index > 0 && hoveredId === filteredDesigners[index - 1].id;
-
                 return (
                   <div
                     key={designer.id}
                     className={`
                       cursor-pointer border-t transition-colors duration-200
-                      ${isHovered || isPreviousHovered ? 'border-[#1C1C1C]' : 'border-[#D2D2D2]'}
+                      ${isHovered || isPreviousHovered ? 'border-[#1C1C1C]' : 'border-[#D2D2D2]'} px-4 lg:px-0
                     `}
                     onMouseEnter={() => setHoveredId(designer.id)}
                     onMouseLeave={() => {
@@ -141,7 +147,7 @@ export default function DesignersPage() {
                     }}
                     onClick={() => handleDesignerClick(designer.id)}
                   >
-                    <div className="p-4 lg:py-5 lg:px-2">
+                    <div className="py-4 lg:py-5 lg:px-2">
                       <div className="flex items-start justify-start gap-x-18 lg:gap-x-[200px]">
                         <div className="flex items-start gap-30">
                           <span className={`${suitMedium.className} w-8 text-[#1C1C1C] text-[13px] lg:text-[24px] lg:w-auto transition-all ${isHovered ? 'font-[600]' : 'font-[400]'}`}>{designer.id}</span>   
